5380c6f9a7ebc0567a349847189fd53e64745ebd
Hampus Fristedt <Hampus Fristedt>
2013-09-18 08:29:04 +0200

    First commit.

---
+++ prolog/lab2/t9v2.pl
@@ -0,0 +1,57 @@
+digit_char(2,0'a). %97
+digit_char(2,0'b). 
+digit_char(2,0'c). 
+digit_char(3,0'd). %100
+digit_char(3,0'e).
+digit_char(3,0'f).
+digit_char(4,0'g). %103
+digit_char(4,0'h).
+digit_char(4,0'i).
+digit_char(5,0'j). %106
+digit_char(5,0'k).
+digit_char(5,0'l).
+digit_char(6,0'm). %107
+digit_char(6,0'n).
+digit_char(6,0'o).
+digit_char(7,0'p). %110
+digit_char(7,0'q).
+digit_char(7,0'r).
+digit_char(7,0's). 
+digit_char(8,0't). %114
+digit_char(8,0'u).
+digit_char(8,0'v). 
+digit_char(9,0'w). %117
+digit_char(9,0'x). 
+digit_char(9,0'y).
+digit_char(9,0'z).
+
+t9(Code, Words, Word) :-
+	decode(Code, Words, Word),
+	member(Word, Words). % FUSK
+
+decode([], _, []).
+decode([Digit|Code], Words, [Char|Word]):-
+	digit_char(Digit, Char),
+	memberchk([Char|_], Words), 
+	filter(Words, Char, Filtered),
+	decode(Code, Filtered, Word).
+
+filter([], _, []).
+filter([[Char|Word]|Rest], Char, [Word|Filtered]) :-
+	filter(Rest, Char, Filtered), !.
+filter([_|Rest], Char, Filtered) :-
+	filter(Rest, Char, Filtered).
+
+test:-
+	words(Words),
+	statistics(runtime,_),
+	t9([2, 2], Words, Word),
+	statistics(runtime,[_, Time]),
+	format("one solution is \"~s\", found in ~w ms~n" ,[Word, Time]).
+
+test2 :-
+	A = [[2, 1, 4, 6], [2, 5, 4, 6], [4, 3, 5, 9]],
+	X = [1|_],
+	memberchk(X, A).
+
+	
---
+++ prolog/lab2/t9v3.pl
@@ -0,0 +1,43 @@
+t9(Code, Tree, Word) :-
+	decode(Code, Tree, Word).
+
+decode([], _, []).
+decode([Digit|Code], Tree, [Char|Word]):-
+	digit_char(Digit, Char),
+	memberchk(branch(Char, Sub), Tree),
+	decode(Code, Sub, Word).
+
+insert_word([], _).
+insert_word([Char|Word], Tree):-
+	memberchk(branch(Char, Branches), Tree), !,
+	insert_word(Word, Branches).
+
+insert_all([], _).
+insert_all([Word|Words], Tree) :-
+	insert_word(Word, Tree),
+	insert_all(Words, Tree).
+
+done([]):- !.
+done([branch(_, Branches)|Rest]):-
+	done(Branches),
+	done(Rest).
+
+build(Words, Tree) :-
+	insert_all(Words, Tree),
+	done(Tree).
+
+test:-
+	words(Words),
+	statistics(runtime,_),
+	build(Words, Tree),!,
+	statistics(runtime,[_,Build]),
+	t9([3,2,7,8], Tree, Word),
+	statistics(runtime,[_, Time]),
+	format("one solution is \"~s\", found in ~w ms, (building of tree in ~w ms)~n" ,[Word, Time, Build]).
+
+% test :-
+% 	T = [branch(0'd, [branch(0'o, []),
+% 			  branch(0'i, [])]),
+% 	     branch(0'r, [branch(0'e, [branch(0'm, [])])])],
+% 	decode([7, 3, 6], T, W),
+% 	write(W).
