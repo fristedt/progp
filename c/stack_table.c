10d1ff079b49f0b56658117ebd19a4fcc3cf9673
Hampus Fristedt <Hampus Fristedt>
2013-09-24 13:36:59 +0200

    Fixed printf stuff and made stack tables.

--- c/stack_table.c
+++ c/stack_table.c
@@ -8,11 +8,11 @@ int main() {
   char chars[5] = "FOUR";
   float floaty = 1.23456;
   printf("%12s%12s%14s%12s\n", "Name", "Value", "Size", "Adress");
-  printf("%12s%12d%14d%p\n", "long_long1", long_long1, sizeof(long_long1), &long_long1);
-  printf("%12s%12d%14d%p\n", "long_long2", long_long2, sizeof(long_long2), &long_long2);
-  printf("%12s%12d%14d%p\n", "uint1", uint1, sizeof(uint1), &uint1);
-  printf("%12s%12d%14d%p\n", "uint2", uint2, sizeof(uint2), &uint2);
-  printf("%12s%12d%14d%p\n", "chars", chars, sizeof(chars), &chars);
-  printf("%12s%12f%14d%p\n", "floaty", floaty, sizeof(floaty), &floaty);
+  printf("%12s%12lld%14ld%16p\n", "long_long1", long_long1, sizeof(long_long1), &long_long1);
+  printf("%12s%12lld%14ld%16p\n", "long_long2", long_long2, sizeof(long_long2), &long_long2);
+  printf("%12s%12d%14ld%16p\n", "uint1", uint1, sizeof(uint1), &uint1);
+  printf("%12s%12d%14ld%16p\n", "uint2", uint2, sizeof(uint2), &uint2);
+  printf("%12s%12s%14ld%16p\n", "chars", chars, sizeof(chars), &chars);
+  printf("%12s%12f%14ld%16p\n", "floaty", floaty, sizeof(floaty), &floaty);
   return 0;
 }
