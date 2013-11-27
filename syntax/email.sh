a44c275b446a1f57350c8bdc7b10891af2825326
Hampus Fristedt <hampus.fristedt93@gmail.com>
2013-11-27 14:37:39 +0100

    Now passes the new test cases.

--- syntax/email.sh
+++ syntax/email.sh
@@ -2,23 +2,33 @@
 #[A-Za-z0-9\!\#\$\%\*\/\?\|\^\{\}\`\~\&\'\+\-\=\_]+@[a-z0-9]+\.com\|org\|net\|nu\|se]] ]]
 
 # lokal=
-regex="^([A-Za-z0-9!#$%*\/?|^{}\`~&'+=_\-]+)@([a-z0-9]+\.)+(com|org|net|nu|se)$"
-# regex="[a-z\-]+@([a-z0-9]+\.)+(com|org|net|nu|se)$"
+regex="^([A-Za-z0-9!#$%*/?|^{}\`~&'+=_-]+\.)?+([A-Za-z0-9!#$%*/?|^{}\`~&'+=_-]+)@([a-z0-9]+\.)+(com|org|net|nu|se)$"
+# regex="^([A-Za-z0-9!#$%*\/?|^{}\`~&'+=_\-]+.)?+([A-Za-z0-9!#$%*\/?|^{}\`~&'+=_\-]+)@([a-z0-9]+\.)+(com|org|net|nu|se)$"
 
 echo "BRA"
 exec 3<bra.txt
-while read -u3 line
+while read -r -u3 line
 do
  if [[ $line =~ $regex ]]; then
-    echo "true: $line"
+    echo true: $line
   else
-    echo "false: $line"
+    echo false: $line
   fi
 done
 
 echo -e "\nDUMT"
 exec 3<dumt.txt
-while read -u3 line
+while read -r -u3 line
+do
+ if [[ $line =~ $regex ]]; then
+    echo true: $line
+  else
+    echo false: $line
+  fi
+done
+
+echo -e "\nNu kan du ange dina egna e-postadresser."
+while read -r line
 do
  if [[ $line =~ $regex ]]; then
     echo "true: $line"
