218a982e9d99732c54e6b1bebc6e0e4d7f86d2f0
Hampus Fristedt <hampus.fristedt93@gmail.com>
2013-12-10 13:00:11 +0100

    Done!

--- syntax/lab2/dansk.hs
+++ syntax/lab2/dansk.hs
@@ -11,7 +11,7 @@ dansk :: String -> Int
 dansk word = en2ni (words word)
 
 en2ni :: [String] -> Int
-en2ni ("nul":tail)  = 0
+en2ni ("nul":tail)  = 0 + (mer tail)
 en2ni ("en":tail)   = 1 + (mer tail)
 en2ni ("et":tail)   = 1 + (mer tail)
 en2ni ("to":tail)   = 2 + (mer tail)
@@ -22,54 +22,61 @@ en2ni ("seks":tail) = 6 + (mer tail)
 en2ni ("syv":tail)  = 7 + (mer tail)
 en2ni ("otte":tail) = 8 + (mer tail)
 en2ni ("ni":tail)   = 9 + (mer tail)
-en2ni (word:tail)   = ti2nitten word
+en2ni words   = ti2nitten words
 
-ti2nitten :: String -> Int
-ti2nitten "ti"      = 10
-ti2nitten "ellve"   = 11
-ti2nitten "tolv"    = 12
-ti2nitten "tretten" = 13
-ti2nitten "fjorten" = 14
-ti2nitten "femten"  = 15
-ti2nitten "seksten" = 16
-ti2nitten "sytten"  = 17
-ti2nitten "atten"   = 18
-ti2nitten "nitten"  = 19
-ti2nitten word      = tyve2halvfems [word]
+ti2nitten :: [String] -> Int
+ti2nitten ["ti"]      = 10
+ti2nitten ["ellve"]   = 11
+ti2nitten ["tolv"]    = 12
+ti2nitten ["tretten"] = 13
+ti2nitten ["fjorten"] = 14
+ti2nitten ["femten"]  = 15
+ti2nitten ["seksten"] = 16
+ti2nitten ["sytten"]  = 17
+ti2nitten ["atten"]   = 18
+ti2nitten ["nitten"]  = 19
+ti2nitten word      = tyve2halvfems word
 
 mer :: [String] -> Int
+mer []          = 0
+mer ("":tail)   = 0 
 mer ("og":tail) = tyve2halvfems tail
-mer _           = 0 
+mer asdf        = error "CUT IT THE FUCK AUS!"
 
 tyve2halvfems :: [String] -> Int
 tyve2halvfems ("tyve":tail)      = 20
 tyve2halvfems ("tredive":tail)   = 30
 tyve2halvfems ("fyrre":tail)     = 40
-tyve2halvfems ("fyrretyve":tail) = 50
+tyve2halvfems ("fyrretyve":tail) = 40
 tyve2halvfems words              = halvtreds2halvfems words
           
 halvtreds2halvfems :: [String] -> Int
-halvtreds2halvfems ("halv":tail) = (-10 + halvmult (last tail))
+halvtreds2halvfems ("halv":tail) = 20*(halvmult tail) - 10 
+halvtreds2halvfems words = 20*(mult words)
 
-mult :: String -> Int
-mult "tres" = 60
-mult "firs" = 80
-mult "fjerds" = 80
+mult :: [String] -> Int
+mult ("tres":tail) = 3 + (emfas tail)
+mult ("firs":tail) = 4 + (emfas tail)
+mult ("fjerds":tail) = 4 + (emfas tail)
+mult fuckerdoodle = error "NaN, bitch!"
 
-halvmult :: String -> Int
-halvmult "fems" = 100
+halvmult :: [String] -> Int
+halvmult ("fems":tail) = 5 + (emfas tail)
 halvmult word = mult word
 
-emfas :: String -> Int
+emfas :: [String] -> Int
 emfas word
-  | word == "sindstyve" = 20
-  | word == ""          = 20
+  | word == ["sindstyve"] = 0
+  | word == []            = 0
   | otherwise           = error "Inte okej!"
 
--- main :: IO ()
+test = do
+  good <- readFile "dr.txt"
+  bad <- readFile "dr-fel.txt"
+  mapM_ (print . dansk) (lines good)
+
 main = do
-  z <- readFile "dr.txt"
-  -- print (lines z)
-  -- map (print (dansk)) (lines z)
-  mapM_ (print . dansk) (lines z)
-  -- print dansk)a
+  forever $ do
+    line <- getLine
+    putStrLn $ show $ dansk line
+  
