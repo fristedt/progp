144c17b3bb34b6106fdff767b7610a79f9bf176b
Hampus Fristedt <hampus.fristedt93@gmail.com>
2014-05-12 14:39:24 +0200

    Solution seems to work. Need to change return type and document. Also add time out if avaliable.

--- prolog/lab4/main.pl
+++ prolog/lab4/main.pl
@@ -1,5 +1,6 @@
 :- use_module(library(clpfd)).
 
+% Returns N binary indicator variables. Credit goes to author of assignment.
 gen_var(0, []). 
 gen_var(N, [X|Xs]) :- 
   N > 0, 
@@ -7,22 +8,22 @@ gen_var(N, [X|Xs]) :-
   gen_var(M, Xs), 
   X in 0..1. % The domain of X is constrained to 0/1
 
+% We get the sum of our indicator variables and tell Prolog to give us the
+% solution with the smallest sum first.
 choose_primers(NumberOfPrimers, Genes, I) :- 
   gen_var(NumberOfPrimers, I),
   iterate_genes(Genes, I),
-  sum(I, #=, S),
-  labeling([min(S)], I).
+  sum(I, #=, S),         
+  labeling([min(S)], I). 
 
 iterate_genes([], _).
 iterate_genes([pri(_, Primers)|Tail], I) :-
-  length(Primers, Length),
   primer_indicators(Primers, I, Indicators),
-  sum(Indicators, #=, 1),
-  iterate_genes(Tail).
+  sum(Indicators, #>=, 1),
+  iterate_genes(Tail, I).
 
-primer_indicators([], _, _).
+primer_indicators([], _, []).
 primer_indicators([Primer|Tail], I, [Indicator|Indicators]) :-
   element(Primer, I, Indicator),
   primer_indicators(Tail, I, Indicators).
-
 
